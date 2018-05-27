11d3e67f2280edadb983211c3698aa0b5b21d31c
Anton Golub <mailbox@antongolub.ru>
2018-05-27 16:59:32 +0300

    test(common):  add tests for  `complete` and `fail` helpers

--- test/common.js
+++ test/common.js
@@ -1,4 +1,4 @@
-import {assert, assertFn} from '../src/common'
+import {assert, assertFn, complete, fail, failOnCancel, REJECTED_ON_CANCEL} from '../src/common'
 
 describe('assert', () => {
   it('does nothing if condition looks truthy', () => {
@@ -18,5 +18,44 @@ describe('assertFn', () => {
   it('requires target to be a function', () => {
     expect(assertFn(() => {})).toBeUndefined()
     expect(() => assertFn({})).toThrow('Target must be a function')
+  })
+})
+
+describe('complete', () => {
+  it('resolves a promise with target fn invocation result', done => {
+    const foo = 'foo'
+    const fn = v => v
+    const p = new Promise((resolve, reject) => complete(resolve, fn, [foo]))
+
+    p.then(v => {
+      expect(v).toBe(foo)
+      done()
+    })
+  })
+})
+
+describe('fail', () => {
+  it('constructs an error and invokes `reject`', done => {
+    const p = new Promise((resolve, reject) => fail(reject, 'foo'))
+
+    p.catch(e => {
+      expect(e).toBeInstanceOf(Error)
+      expect(e.message).toBe('foo')
+
+      done()
+    })
+  })
+})
+
+describe('failOnCancel', () => {
+  it('rejects with `REJECTED_ON_CANCEL`', done => {
+    const p = new Promise((resolve, reject) => failOnCancel(reject))
+
+    p.catch(e => {
+      expect(e).toBeInstanceOf(Error)
+      expect(e.message).toBe(REJECTED_ON_CANCEL)
+
+      done()
+    })
   })
 })
