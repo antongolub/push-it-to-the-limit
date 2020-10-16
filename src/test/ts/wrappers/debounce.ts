6fe78d0c78782f9c5c4df20c8f3a056c937d9503
Choro Abdymanapov <c.abdymanapov@qiwi.com>
2020-10-16 09:47:30 +0300

    style: removed unused import

--- src/test/ts/wrappers/debounce.ts
+++ src/test/ts/wrappers/debounce.ts
@@ -2,7 +2,6 @@ import { debounce, REJECTED_ON_CANCEL } from '../../../main/ts'
 import 'babel-polyfill'
 import { ITarget, IWrapperOpts } from '../../../main/ts/interface'
 import { Limiter } from '../../../main/ts/limiter'
-import * as LimiterModule from '../../../main/ts/limiter'
 
 describe('debounce', () => {
   it('wrapper returns function', () => {
